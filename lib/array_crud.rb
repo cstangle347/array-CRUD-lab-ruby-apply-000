e7f4e9b03c66ed73a2db232f8c8a9b5a9881098f
Christopher Stangle <christopherstangle@gmail.com>
2019-03-18 16:10:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
- days = ["Monday", "Tuesday", "Wednesday", "Friday"]
+ days = ["Monday", "Tuesday", "Wednesday", "Thursday"]
 end
 
 def add_element_to_end_of_array(array, element)
-  days.push("Saturday")
+  days.push("Friday")
 end
 
 def add_element_to_start_of_array(array, element)
